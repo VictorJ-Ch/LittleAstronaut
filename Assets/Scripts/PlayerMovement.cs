f1c852e6deb93d36e233e8751022f82c01d6b4e3
Kaseem <kaseem@users.noreply.github.com>
2024-08-29 15:09:45 -0600

    PL movement
    + Script
    + Capsule 2D Asset

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 touchPosition, direction;
+    Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    Touch playerTouch;
+    public Camera camera;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(Input.touchCount > 0)
+        {
+            //Touch detection
+            playerTouch = Input.GetTouch(0);
+            touchPosition = camera.ScreenToWorldPoint(playerTouch.position);
+            //Touch movement
+            touchPosition.z = 0.0f;
+            direction = touchPosition - transform.position;
+            rb.linearVelocity = new Vector2(direction.x, direction.y) * moveSpeed * Time.deltaTime;
+            //Validator What if is not touching
+            if (playerTouch.phase == TouchPhase.Ended)
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+    }
+}
