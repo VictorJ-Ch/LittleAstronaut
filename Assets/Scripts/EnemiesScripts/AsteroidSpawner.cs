ef8ba139b32091b4ad13905c622cfe47e4175eb3
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-09-11 18:23:45 -0600

    LittleAstronaut V1
    + Astroid Spawner.
    + UI bug fixes

--- Assets/Scripts/EnemiesScripts/AsteroidSpawner.cs
+++ Assets/Scripts/EnemiesScripts/AsteroidSpawner.cs
@@ -1,24 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Security.Principal;
 using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    public GameObject asteroidPrefab;
-    public float spawnRate = 2.0f;
-    public float spawnRadius = 10.0f;
-    void Start()
+    public float timeToSpawn;
+    public GameObject[] prefabs;
+    public Transform[] spawner;
+    List <GameObject> spawnedAsteroids = new List<GameObject>();
+    IEnumerator Start()
     {
-        InvokeRepeating("SpawnAsteroid", 0.0f, spawnRate);
+        while(true)
+        {
+            GameObject newAsteroid = Instantiate
+            (
+                prefabs[Random.Range(0,prefabs.Length)],
+                spawner[Random.Range(0,spawner.Length)].position,
+                Quaternion.identity
+            );
+            spawnedAsteroids.Add(newAsteroid);
+            //Remove first clone
+            StartCoroutine(RemoveFirstClone());
+            yield return new WaitForSeconds (timeToSpawn);
+        }
     }
-
-    void Update()
+    IEnumerator RemoveFirstClone()
     {
-        
-    }
-
-    void SpawnAsteroid()
-    {
-        Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius;
-        Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(10.0f);
+        if(spawnedAsteroids.Count > 0)
+        {
+            GameObject asteroidToRemove = spawnedAsteroids[0];
+            spawnedAsteroids.RemoveAt(0);
+            Destroy(asteroidToRemove);
+        }
     }
 }
